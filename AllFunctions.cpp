8a5a05f505d90f28e371a11109c5d18ae959e67f
AntoninaNayden <121058964+AntoninaNayden@users.noreply.github.com>
2022-12-30 17:44:41 +0300

    add all functions

--- AllFunctions.cpp
+++ AllFunctions.cpp
@@ -27,3 +27,122 @@ enum mode{
     Prog=4,
     Conv=5
 };
+#include <iostream>
+#include <cmath>
+double Addition(double a, double b) {
+    return a + b;
+}
+
+double Subtraction(double a, double b) {
+    return a - b;
+}
+
+double Multiplication(double a, double b) {
+    return a * b;
+}
+
+double Division(double a, double b) {
+    return a / b;
+}
+
+double Pow(double a, double b) {
+    if (b != 0) {
+        return pow(a, b);
+    }
+    else return 1;
+}
+
+double Negative(double a) {
+    return -a;
+}
+
+double Exponent(double a) {
+    return Pow(2.718, a);
+}
+
+double Ln(double a) {
+    return log(a);
+}
+
+double Log10(double a) {
+    return log10(a);
+}
+
+double Log(double a, double b) {
+    return log(b) / log(a);
+}
+
+double Sqrt(double a) {
+    return Pow(a, 0.5);
+}
+
+double Abs(double a) {
+    if (a >= 0) {
+        return a;
+    }
+    else return -a;
+}
+
+double Factorial(double a) {
+    while (a > 1) {
+        return a * Factorial(a - 1);
+    }
+    if (a == 1) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+double Sinus(double a) {
+    while (a > 6.283185307) {
+        a -= 6.283185307;
+    }
+    double temp = 0;
+    int n = 1;
+    double b = a;
+    while (Abs(a) > 0.000001) {
+        temp += a;
+        a = a * (-1) * b * b / (2 * n + 1) / 2 / n;
+        n++;
+    }
+    return temp;
+}
+
+double Cosinus(double a) {
+    return 1 - Pow(Sinus(a), 2);
+}
+
+double Tangens(double a) {
+    return Sinus(a) / Cosinus(a);
+}
+
+double Cotangens(double a) {
+    return Cosinus(a) / Sinus(a);
+}
+
+double Ceil (double a) {
+    if ((a - a / 10) != 0 ) {
+        return a + 1;
+    }
+    else {
+        return a;
+    }
+}
+
+double Floor(double a) {
+    if ((a - a / 10) != 0) {
+        return a;
+    }
+    else {
+        return a;
+    }
+}
+
+double Inverse(double a) {
+    return 1 / a;
+}
+
+
+
